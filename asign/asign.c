99b8cfe705de1aa3f294d1a63d118d2a89afbf1d
garlockhart <official.garlynugraha@gmail.com>
2021-11-18 21:31:05 +0700

    Make asign

---
+++ asign/asign.c
@@ -0,0 +1,25 @@
+/*
+Nama File		: asign.c
+Deskripsi		: Asign nilai integer dan print
+Dibuat Oleh		: Dosen DDP / 132 231 592
+Tanggal Dibuat	: 30/08/2001
+Diedit Oleh		: Garly Nugraha
+Tanggal Diedit	: 17/11/2021
+*/
+
+#include <stdio.h>
+
+int main()
+{
+//	Kamus Data
+	int i;
+	
+//	Program
+	printf("Hello");
+	printf("\n");
+	
+	i = 5;
+	printf("Ini nilai i : %d", i);
+	
+	return 0;
+}
