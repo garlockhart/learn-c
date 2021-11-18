151cb48092eb33ea16ce038f1d412b580efd40c2
garlockhart <official.garlynugraha@gmail.com>
2021-11-18 21:39:54 +0700

    Make kasus

---
+++ incr/incr.c
@@ -0,0 +1,23 @@
+/*
+Nama File		: incr.c
+Deskripsi		: Efek dari operator
+Dibuat Oleh		: Dosen DDP / 132 231 592
+Tanggal Dibuat	: 30/08/2001
+Diedit Oleh		: Garly Nugraha
+Tanggal Diedit	: 17/11/2021
+*/
+
+#include <stdio.h>
+
+int main()
+{
+//	Kamus Data
+	int i, j;
+	
+//	Program
+	i = 3;
+	j = i++;
+	printf("Nilai i : %d \nNilai j : %d", ++i, j);
+	
+	return 0;
+}
