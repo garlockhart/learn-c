7dd0cf538a98b8f33aa30ed6f099a0c2ce8c088a
garlockhart <official.garlynugraha@gmail.com>
2021-11-18 21:28:55 +0700

    Make asgdll

---
+++ asgdll/asgdll.c
@@ -0,0 +1,27 @@
+/*
+Nama File		: asgdll.c
+Deskripsi		: Asignment float dan print
+Dibuat Oleh		: Dosen DDP / 132 231 592
+Tanggal Dibuat	: 30/08/2001
+Diedit Oleh		: Garly Nugraha
+Tanggal Diedit	: 17/11/2021
+*/
+
+#include <stdio.h>
+
+int main()
+{
+//	Kamus Data
+	float f;
+	long double fll;
+	
+//	Program
+	f = 20.0f;
+	fll = 10.0L;
+	
+//	Program Untuk Menampilkan Hasil Assign Ke Layar
+	printf("Nilai f = %f", f);
+	printf("\n");
+	printf("Nilai fll = %d", fll);
+	return 0;
+}
